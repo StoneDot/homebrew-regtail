7891360480c7000c841fbc04be3c0e28c1ce4532
Hiroaki Goto <goto.hiroaki.10050@colopl.co.jp>
2019-04-05 15:22:49 +0900

    Bump version up to v0.1.0

--- regtail.rb
+++ regtail.rb
@@ -1,8 +1,8 @@
 class Regtail < Formula
   desc "Regex base tail written in Rust"
   homepage "https://github.com/StoneDot/regtail"
-  url "https://github.com/StoneDot/regtail/archive/v0.0.2.tar.gz"
-  sha256 "199852f842ca5b389845554d271655adf978c01c5c99f95ab664476a85fbb0c7"
+  url "https://github.com/StoneDot/regtail/archive/v0.1.0.tar.gz"
+  sha256 "ef5f15980427d9e730aa633ae6846099681d67331ec2062120aaf3e3263b4fc5"
   depends_on "rust" => :build
   head "https://github.com/StoneDot/regtail.git"
 
